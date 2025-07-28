01d46c9389907e295e93a87b0aaaa1d8a4b2fee8
AviKoduri <saiavinashkoduri@gmail.com>
2025-07-28 21:36:07 +0530

    added auth

--- routes/post.js
+++ routes/post.js
@@ -4,17 +4,18 @@ const Post = require("../models/post"); // Correct
 const authMiddleware = require("../utils/authmiddleware");
 
 //get all
-router.get("/",async (req, res) => {
+router.get("/",authMiddleware,async (req, res) => {
   try {
      const skip = parseInt(req.query.skip, 10) || 0;
     const limit = parseInt(req.query.limit, 10) || 10;
-    // const filter = { organisation: { $in: req.user.organisations } };
-
-    const posts = await Post.find().skip(skip)
+    const filter = { organisation: { $in: req.user.organisations } };
+console.log("enter to post",filter)
+    const posts = await Post.find(filter).skip(skip)
     .populate("author")
-      .limit(limit)
-      .sort({ createdAt: -1 });
-      const totalCount = await Post.countDocuments();
+    .limit(limit)
+    .sort({ createdAt: -1 });
+    console.log("posts",posts)
+      const totalCount = await Post.countDocuments(filter);
 res.json({ posts, totalCount });
 
   } catch (error) {
